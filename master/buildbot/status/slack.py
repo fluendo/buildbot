8edf08841ebcbcc9bd964a5bbdc881dae1b77b98
Víctor Guzmán <vguzman@fluendo.com>
2017-06-14 19:29:37 +0200

    Update slack status plugin to 0.9.7
    Copied from https://github.com/hotgloupi/docker-bot/blob/4c26635dbdfb17705b1f5e0687a13500b87a8172/src/dockerbot/data/slack.py

--- master/buildbot/status/slack.py
+++ master/buildbot/status/slack.py
@@ -1,127 +1,172 @@
-from buildbot.status.base import StatusReceiverMultiService
-from buildbot.status.builder import Results, SUCCESS
-import requests
+
+## This file comes from https://github.com/hotgloupi/docker-bot/blob/4c26635dbdfb17705b1f5e0687a13500b87a8172/src/dockerbot/data/slack.py
+
+from buildbot.reporters.http import HttpStatusPushBase
+from buildbot.util import httpclientservice
+from buildbot.util.logger import Logger
+from buildbot.reporters import utils
+from buildbot.process.results import CANCELLED
+from buildbot.process.results import EXCEPTION
+from buildbot.process.results import FAILURE
+from buildbot.process.results import SUCCESS
+from buildbot.process.results import WARNINGS
+from buildbot.process.results import Results
+
+from twisted.internet import defer
 import json
 
+log = Logger()
 
-class SlackStatusPush(StatusReceiverMultiService):
+class SlackStatusPush(HttpStatusPushBase):
     """
     Sends messages to a Slack.io channel when each build finishes with a handy
     link to the build results.
     """
 
-    def __init__(self, weburl,
-                 localhost_replace=False, username=None,
-                 icon=None, notify_on_success=True, notify_on_failure=True,
-                 **kwargs):
+    name = "SlackStatusPush"
+
+    @defer.inlineCallbacks
+    def reconfigService(self,
+                        weburl,
+                        username = None,
+                        icon = None,
+                        templates = None,
+                        **kwargs):
         """
         Creates a SlackStatusPush status service.
 
         :param weburl: Your Slack weburl
-        :param localhost_replace: If your Buildbot web fronted doesn't know
-            its public address it will use "localhost" in its links. You can
-            change this by setting this variable to true.
         :param username: The user name of the "user" positing the messages on
             Slack.
         :param icon: The icon of the "user" posting the messages on Slack.
-        :param notify_on_success: Set this to False if you don't want
-            messages when a build was successful.
-        :param notify_on_failure: Set this to False if you don't want
-            messages when a build failed.
         """
 
-        StatusReceiverMultiService.__init__(self)
+        yield HttpStatusPushBase.reconfigService(self, **kwargs)
+        self._http = yield httpclientservice.HTTPClientService.getService(self.master, weburl)
 
         self.weburl = weburl
-        self.localhost_replace = localhost_replace
         self.username = username
         self.icon = icon
-        self.notify_on_success = notify_on_success
-        self.notify_on_failure = notify_on_failure
-        self.watched = []
 
-    def setServiceParent(self, parent):
-        StatusReceiverMultiService.setServiceParent(self, parent)
-        self.master_status = self.parent
-        self.master_status.subscribe(self)
-        self.master = self.master_status.master
+        self.templates = {}
+        if templates is None:
+            templates = {}
+        for t in ['repository', 'branch', 'revision']:
+            self.templates[t] = templates.get(t, '{%s}' % t)
 
-    def disownServiceParent(self):
-        self.master_status.unsubscribe(self)
-        self.master_status = None
-        for w in self.watched:
-            w.unsubscribe(self)
-        return StatusReceiverMultiService.disownServiceParent(self)
+    @defer.inlineCallbacks
+    def buildFinished(self, key, build):
+        yield utils.getDetailsForBuild(
+            self.master,
+            build,
+            wantProperties = True,
+            wantSteps = True,
+            wantPreviousBuild = False,
+            wantLogs = False
+        )
+        prop = lambda name: build['properties'].get(name, [None])[0]
 
-    def builderAdded(self, name, builder):
-        self.watched.append(builder)
-        return self  # subscribe to this builder
+        build_url = build['url']
+        source_stamps = build['buildset']['sourcestamps']
+        branch = prop('branch')
+        buildername = prop('buildername')
+        variant_name = prop('variant_name')
+        buildername = prop('buildername')
+        buildnumber = prop('buildnumber')
+        worker = prop('workername')
+        got_rev = prop('got_revision')
+        rev = prop('revision')
+        repository = prop('repository')
+        repository_url = prop('repository_url')
+        project = prop('project')
+        blamelist = yield utils.getResponsibleUsersForBuild(
+            self.master,
+            build['buildid']
+        )
+        responsible_users = '\n'.join(blamelist)
+        status = Results[build['results']]
 
-    def buildFinished(self, builder_name, build, result):
-        if not self.notify_on_success and result == SUCCESS:
-            return
-
-        if not self.notify_on_failure and result != SUCCESS:
-            return
-
-        build_url = self.master_status.getURLForThing(build)
-        if self.localhost_replace:
-            build_url = build_url.replace("//localhost", "//{}".format(
-                self.localhost_replace))
-
-        source_stamps = build.getSourceStamps()
-        branch_names = ', '.join([source_stamp.branch for source_stamp in source_stamps])
-        repositories = ', '.join([source_stamp.repository for source_stamp in source_stamps])
-        responsible_users = ', '.join(build.getResponsibleUsers())
-        revision = ', '.join([source_stamp.revision for source_stamp in source_stamps])
-        project = ', '.join([source_stamp.project for source_stamp in source_stamps])
-
-        if result == SUCCESS:
-            status = "Success"
+        if build['results'] == SUCCESS:
             color = "good"
+        elif build['results'] == FAILURE:
+            color = "#EE3435"
         else:
-            status = "Failure"
-            color = "failure"
+            color = '#AB12EF'
 
-        message = "New Build for {project} ({revision})\nStatus: *{status}*\nBuild details: {url}".format(
-            project=project,
-            revision=revision,
-            status=status,
-            url=build_url
+        message = "Build <{url}|#{buildnumber} {buildername} {variant_name} on {worker}> finished".format(
+            url = build_url,
+            buildnumber = buildnumber,
+            buildername = buildername or '',
+            variant_name = variant_name or '',
+            worker = worker,
         )
 
-        fields = []
+        fields = [
+            {
+                'title': 'Status',
+                'value': status,
+                'short': True,
+            },
+            {
+                "title": "Repository",
+                "value": repository,
+                "short": False
+            },
+            {
+                "title": "Project",
+                "value": project,
+                "short": True
+            }
+        ]
         if responsible_users:
             fields.append({
-                "title": "Commiters",
-                "value": responsible_users
+                "title": "Responsible users",
+                "value": responsible_users,
+                'rev_short': True,
             })
 
-        if repositories:
+        if branch:
             fields.append({
-                "title": "Repository",
-                "value": repositories,
+                "title": "Branch",
+                "value": self.templates['branch'].format(
+                    repository = repository,
+                    branch = branch,
+                ),
                 "short": True
             })
 
-        if branch_names:
+        if rev:
             fields.append({
-                "title": "Branch",
-                "value": branch_names,
+                "title": "Revision",
+                "value": self.templates['revision'].format(
+                    repository = repository,
+                    revision = rev,
+                    revision_short = rev[:8]
+                ),
+                "short": True
+            })
+
+        if got_rev and got_rev != rev:
+            fields.append({
+                "title": "Got Revision",
+                "value": self.templates['revision'].format(
+                    repository = repository,
+                    revision = got_rev,
+                    revision_short = got_rev[:8]
+                ),
                 "short": True
             })
 
         payload = {
-            "text": " ",
             "attachments": [
               {
-                "fallback": message,
-                "text": message,
-                "color": color,
-                "mrkdwn_in": ["text", "title", "fallback"],
-                "fields": fields
+                  "text": message,
+                  "color": color,
+                  "mrkdwn_in": ["text", "title", "fallback", "fields"],
+                  "fields": fields
               }
-            ]
+            ],
+            'mrkdwn': True,
         }
 
         if self.username:
@@ -133,4 +178,8 @@ class SlackStatusPush(StatusReceiverMultiService):
             else:
                 payload['icon_url'] = self.icon
 
-        requests.post(self.weburl, data=json.dumps(payload))
+        response = yield self._http.post("", json=payload)
+        if response.code != 200:
+            content = yield response.content()
+            log.error("{code}: unable to upload status: {content}",
+                      code=response.code, content=content)
